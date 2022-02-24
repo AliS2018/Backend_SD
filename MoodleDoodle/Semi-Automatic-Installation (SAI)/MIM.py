2bc3f6b037ec3a96bba0162a0e46db23fdea096c
AliS2018 <41644264+AliS2018@users.noreply.github.com>
2022-02-24 20:09:23 +0100

    Add files via upload

--- MoodleDoodle/Semi-Automatic-Installation (SAI)/MIM.py
+++ MoodleDoodle/Semi-Automatic-Installation (SAI)/MIM.py
@@ -2,9 +2,8 @@ import os
 menu_options = {
     '1': 'Update and/or Upgrade OS (packages)',
     '2': 'Install Main Modules',
-    '3': 'Install Additional Modules (Strongly Recommended)',
-    '4': 'Site Configurator v1.0',
-    '5': 'DB Configurator + Site Preparator 2',
+    '3': 'Site Configurator v1.0',
+    '4': 'DB Configurator + Site Preparator 2',
     '0': 'Exit',
 }
 
@@ -54,17 +53,17 @@ def MainModules(p_choice, p_extras):
     if (p_choice == "C" and p_extras == "Y"):
         print("Running Complete Installation Sequence")
         __complete_installation = [ "apt install apache2 -y",
-                                    "apt install php7.4-common -y",
+                                    "apt install php8.0-common -y",
                                     "apt install mysql-client -y",
                                     "apt install mysql-server -y",
-                                    "apt install ghostscript php7.4-pspell php7.4-curl php7.4-gd php7.4-intl php7.4-mysql php7.4-xml php7.4-xmlprc php7.4-xlmrpc php7.4-ldap php7.4-zip php7.4-soap php7.4-mbstring",
+                                    "apt install graphviz aspell php8.0-pspell php8.0-curl php8.0-gd php8.0-intl php8.0-mysql php8.0-xml php8.0-ldap",
                                     ]
         OS_MCE(__complete_installation)
     elif (p_choice == "PW" and p_extras == "Y"):
         print("Running Partial Installation (Web-only) Sequence")
         __partial_installation_web = ["apt install apache2 -y",
                                       "apt install php7.4-common -y",
-                                      "apt install ghostscript php7.4-pspell php7.4-curl php7.4-gd php7.4-intl php7.4-mysql php7.4-xml php7.4-xmlprc php7.4-xlmrpc php7.4-ldap php7.4-zip php7.4-soap php7.4-mbstring"
+                                      "apt install graphviz aspell php8.0-pspell php8.0-curl php8.0-gd php8.0-intl php8.0-mysql php8.0-xml php8.0-ldap",
                                      ]
         OS_MCE(__partial_installation_web)
     elif (p_choice == "PDB" and p_extras == "N"):
@@ -75,15 +74,6 @@ def MainModules(p_choice, p_extras):
     else:
         os.system("clear")
         print("Action Aborted...")
-def AdditionalModules():
-   
-    yeah = str(input("Install Additional Site Modules (Y/N) (Strongly Recommended!): ")).upper()
-    if(yeah == "Y" or yeah == "YES"):
-        __php_modules = [ "apt install graphviz aspell php8.0-pspell php8.0-curl php8.0-gd php8.0-intl php8.0-mysql php8.0-xml php8.0-ldap" ]
-        OS_MCE(__php_modules)
-    else:
-        print("Action Aborted...")
-        os.system("clear")
 def SiteConfigurator():
 
     __moovable_sed = [  "echo 'Preparing Environment...'",
@@ -92,15 +82,16 @@ def SiteConfigurator():
                         "sleep 2",
                         "mv -v moodle /var/www/html/",
                         "echo 'Generating moodledata directory...'",
-                        "sleep 3"
+                        "sleep 3",
                         "mkdir -p /etc/moodledata",
                         "echo 'Writing permissions to |/etc/moodledata|...'",
                         "chown -R www-data /etc/moodledata",
                         "chmod -R 0777 /etc/moodledata",
                         "chmod -R 0755 /var/www/html/moodle",
                         "echo 'Site Structure configuration is complete!'",
-                        "sleep 1"
-                        "echo 'Please Configure your DB before finished the installation'",
+                        "sleep 1",
+                        "sed -i '/DocumentRoot /var/www/html/moodle//c\DocumentRoot /var/www/html.' /etc/apache2/sites-available/000-default.conf"
+                        "echo 'Please Configure your DB before exiting this Installation Script'",
                         "sleep 5",                    
                         ]  
     OS_MCE(__moovable_sed)
@@ -112,13 +103,13 @@ def DockerConfigurator():
                "sleep 5",
                "echo 'Executing |mysql.yaml|...'",
                "docker-compose -f mysql.yaml up -d",
-               "docker exec -it mysql_moodle /bin/bash",
-               "cd /etc/mysql/mysql.conf.d",
-               "echo 'default_storage_engine = innodb' >> mysqld.conf",
-               "echo 'innodb_file_per_table = 1' >> mysqld.conf",
-               "echo 'innodb_file_format = Barracuda' >> mysqld.conf",
+               "docker exec -it mysql_moodle /bin/bash",        
+               "echo 'Insert following lines when inside MoodleMysql Container starting with command |echo| : '"
+               "echo 'default_storage_engine = innodb' >> /etc/mysql/mysql.conf.d",
+               "echo 'innodb_file_per_table = 1' >> /etc/mysql/mysql.conf.d",
+               "echo 'innodb_file_format = Barracuda' >> /etc/mysql/mysql.conf.d",
                "exit",
-               "docker restart mysql_moodle"]
+               "docker restart mysql_moodle",]
     OS_MCE(__run_file)
 if __name__=='__main__':
     while(True):
@@ -138,8 +129,6 @@ if __name__=='__main__':
             choice = input(("| C | for Complete, | PW | for Partial Web, | PDB | for Partial DB Installation or | X | to Abort the Installation: ")).upper()
             modules = input(("Type Y or N to install extra PHP modules for you Moodle: ")).upper()
             MainModules(choice, modules)
-        elif option == '3':
-            AdditionalModules()
         elif option == '4':
             SiteConfigurator()
         elif option == '5':
